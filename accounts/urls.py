66f1a9c7066be4d6efcfaee160444ae44b495ab1
DMK <dmkshift@gmail.com>
2019-11-10 23:40:13 +0200

    added reset password functions

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('log-out', logout, name="log-out"),
+    path('log-out', log_out, name="log-out"),
     path('sign-in', signin, name="sign-in"),
     path('sign-up', signup, name="sign-up"),
     path('forgot-password', forgot_password, name="forgot-password"),
+    path('password/reset_confirm/<slug:uidb64>/<slug:token>', password_reset_confirm, name="forgot-password"),
     path('activate/<slug:uidb64>/<slug:token>', activate, name="activate"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+from django.core.validators import validate_email
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.hashers import make_password
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
@@ -52,14 +55,47 @@ def signin(request):
         return JsonResponse({"success": False, "error": "there was an error"})
 
 
-def logout(request):
+def log_out(request):
     logout(request)
     return redirect('/')
 
 
+def email_validation(email):
+    try:
+        validate_email(email)
+        return True
+    except ValidationError:
+        return False
+
+
 def forgot_password(request):
-    print(request.POST.dict())
-    return JsonResponse({"message": "OK"})
+    if request.is_ajax():
+        u_email = request.POST.get('email')
+
+        if email_validation(u_email):
+            user = User.objects.filter(email=u_email).first()
+            if user.exists():
+                current_site = get_current_site(request)
+                message = render_to_string('', {
+                    'email': user.email,
+                    'domain': current_site,
+                    'site_name': 'your site',
+                    'uid': urlsafe_base64_encode(force_bytes(user.id)),
+                    'user': user,
+                    'token': default_token_generator.make_token(user),
+                    'protocol': 'http',
+                })
+                subject_template_name = 'registration/password_reset_subject.txt'
+
+                email_template_name = 'registration/password_reset_email.html'
+
+                subject = 'Password recovering'
+
+                email = EmailMessage(subject, message, to=[u_email])
+                email.send()
+                return JsonResponse({"message": "OK"})
+        else:
+            return JsonResponse({"success": False, "error": "there was an error"})
 
 
 def activate(request, uidb64, token):
@@ -74,5 +110,24 @@ def activate(request, uidb64, token):
         user.save()
         # login(request, user)
         return HttpResponse('Your account has been activate successfully')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+def password_reset_confirm(request, uidb64, token):
+    try:
+        uid = force_bytes(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        if request.is_ajax():
+            new_password = request.POST.get('password')
+            user.set_password(new_password)
+            user.save()
+            return HttpResponse('Your password has been upadated successfully')
+        else:
+            return HttpResponse('BAD request')
     else:
         return HttpResponse('Activation link is invalid!')
